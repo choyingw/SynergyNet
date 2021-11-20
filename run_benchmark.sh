02fe0ec945dfeec38effbf17cdc990e7d679e27a
telin0411 <eggnome0411@gmail.com>
2021-11-20 15:15:14 -0800

    updated

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,2 +1,5 @@
+# Rebase from upstream: https://stackoverflow.com/questions/7244321/how-do-i-update-or-sync-a-forked-repository-on-github
+
+
 python3 benchmark.py \
     -w pretrained/best.pth.tar \
