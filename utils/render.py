df61bf1d103f0d5c8d095934fb0c466ce7e71e38
Cho-Ying Wu <choyingw@usc.edu>
2021-11-30 04:29:11 -0800

    incomplete

---
+++ utils/render.py
@@ -0,0 +1,87 @@
+# modified from 3DDFA-V2
+
+import sys
+
+sys.path.append('..')
+
+import cv2
+import numpy as np
+import scipy.io as sio
+
+from Sim3DR import RenderPipeline
+
+def _to_ctype(arr):
+    if not arr.flags.c_contiguous:
+        return arr.copy(order='C')
+    return arr
+tri = sio.loadmat('./train.configs/tri.mat')['tri'] - 1
+tri = _to_ctype(tri.T).astype(np.int32)
+
+
+cfg = {
+    'intensity_ambient': 0.3,
+    'color_ambient': (1, 1, 1),
+    'intensity_directional': 0.6,
+    'color_directional': (1, 1, 1),
+    'intensity_specular': 0.1,
+    'specular_exp': 5,
+    'light_pos': (0, 0, 5),
+    'view_pos': (0, 0, 5)
+}
+
+
+render_app = RenderPipeline(**cfg)
+
+
+def render(img, ver_lst, alpha=0.6, wfp=None):
+    # save solid mesh rendering and alpha overlaying on images
+    overlap = img.copy()
+    for (ver_, clrs) in ver_lst:
+        ver_ = ver_.astype(np.float32)
+        ver = _to_ctype(ver_.T)  # transpose
+        overlap = render_app(ver, tri, overlap)
+    cv2.imwrite(wfp[:-4]+'_solid'+'.png', overlap)
+
+    res = cv2.addWeighted(img, 1 - alpha, overlap, alpha, 0)
+    if wfp is not None:
+        cv2.imwrite(wfp, res)
+        print(f'Save visualization result to {wfp}')
+
+    return res
+
+def render_vert(img, vert, alpha=0.6, wfp=None):
+    #overlap = np.zeros_like(img.copy())
+    overlap = img.copy()
+    vert = vert.astype(np.float32)
+    ver = _to_ctype(vert.T)  # transpose
+    overlap = render_app(ver, tri, overlap)
+    cv2.imwrite(wfp[:-4]+'_s'+'.png', overlap)
+
+    res = cv2.addWeighted(img, 1 - alpha, overlap, alpha, 0)
+
+    if wfp is not None:
+        cv2.imwrite(wfp, res)
+        print(f'Save visualization result to {wfp}')
+
+    return res
+
+def render_texture(img, vert, texture, triangle, alpha=0.6, wfp=None):
+    #overlap = np.zeros_like(img.copy())
+    overlap = img.copy()
+    vert = vert.astype(np.float32)
+    ver = _to_ctype(vert.T)  # transpose'
+    print("A",tri.shape)
+    print("A",tri.dtype)
+    triangle = _to_ctype(triangle.T).astype(np.int32)-1
+    print("B",triangle.shape)
+    print("B",triangle.dtype)
+    overlap = render_app(ver, triangle, overlap, texture=texture)
+    cv2.imwrite(wfp[:-4]+'_s'+'.png', overlap)
+
+    res = cv2.addWeighted(img, 1 - alpha, overlap, alpha, 0)
+
+    if wfp is not None:
+        cv2.imwrite(wfp, res)
+        print(f'Save visualization result to {wfp}')
+
+    return res
